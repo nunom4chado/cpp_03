c63aeead83ce8cfc69d91b3ebef010386f6d262c
nunom4chado <nunom4chado@gmail.com>
2023-10-11 22:01:27 +0100

    adds ex00

---
+++ ex00/main.cpp
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: numartin <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/11 18:53:51 by numartin          #+#    #+#             */
+/*   Updated: 2023/10/11 21:59:51 by numartin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+int main( void ) {
+    ClapTrap a;
+    ClapTrap b("SL4CK-TP");
+
+    ClapTrap c(b);
+    std::cout << c.getName() << std::endl;
+    
+    a.attack("Awesome Jack");
+    a.attack("Awesome Jack");
+    a.attack("Awesome Jack");
+    a.attack("Awesome Jack");
+    a.attack("Awesome Jack");
+    a.attack("Awesome Jack");
+    a.attack("Awesome Jack");
+    a.attack("Awesome Jack");
+    a.attack("Awesome Jack");
+    a.attack(b.getName());
+    a.attack("Awesome Jack");
+
+    std::cout << a.getName() << " has " << a.getEnergy() << " energy left" << std::endl;
+
+    a.takeDamage(4);
+    a.takeDamage(10);
+    a.takeDamage(10);
+    a.takeDamage(10);
+    std::cout << a.getName() << " hp is " << a.getHp() << std::endl;
+    
+    a.beRepaired(24);
+    std::cout << a.getName() << " hp is " << a.getHp() << std::endl;
+
+    c = a;
+    std::cout << c.getName() << " hp is " << c.getHp() << std::endl;
+    
+    return 0;
+}
